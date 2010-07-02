880ae3aa63b22175acefae38ae2910705e54514c
Melchior FRANZ <mfranz@aon.at>
2010-07-03 01:40:36 +0200

    scale instrument face and scale separately (for easier setting of margin)

--- examples/torque.py
+++ examples/torque.py
@@ -14,29 +14,31 @@ __doc__ = """
 try:
 	a = Instrument("torque.svg", 512, 512, "Bo105 torquemeter; " + __version__)
 
-	a.scale(0.85).translate(-18, -18).push()
+	a.scale(0.85).translate(-17, -17).push()
 	a.square(200, '#181818')
+	#a.disc(100, '#181818')
 
+	a.scale(0.92).push()
 	a.angle = lambda x: 230.0 * (x - 60) / 120 - 90
 
 	for i in range(0, 120, 2):
-		a.tick(i, 78, 95, 1)
+		a.tick(i, 82, 99, 1)
 
-	a.arc(0, 80, 91.3, width = 10, color = "#00c000")
+	a.arc(0, 80, 94.5, width = 10, color = "#00c000")
 
 	for i in range(0, 121, 10):
-		a.tick(i, 78, 96, 3)
+		a.tick(i, 81, 99.5, 3)
 
-	a.tick(80, 78, 96, 3, color = "#ffc000")
-	a.tick(110, 68, 96, 3, color = "red")
+	a.tick(80, 81, 99.5, 3, color = "#ffc000")
+	a.tick(110, 70, 99.5, 3.3, color = "red")
 
 	fontsize = 20
-	a.at(-58, 38).text("0", fontsize)
-	a.at(-59, -8).text("20", fontsize)
-	a.at(-37, -45).text("40", fontsize)
-	a.at(0, -58).text("60", fontsize)
-	a.at(37, -45).text("80", fontsize)
-	a.at(55, -8).text("100", fontsize)
+	a.at(-60, 38).text("0", fontsize)
+	a.at(-61, -8).text("20", fontsize)
+	a.at(-39, -46).text("40", fontsize)
+	a.at(0, -61).text("60", fontsize)
+	a.at(39, -46).text("80", fontsize)
+	a.at(56, -8).text("100", fontsize)
 	a.at(50, 38).text("120", fontsize)
 
 	a.at(0, 55).text("%", 17)
@@ -47,6 +49,7 @@ try:
 	a.disc(8, 'black')
 
 	a.disc(0.4, 'red')
+	a.pop()
 	a.pop()
 
 
